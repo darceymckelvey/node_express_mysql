03a70465438a4c4e3f54eef655596365075f59cc
Darcey Mckelvey <darceymckelvey@gmail.com>
2024-11-04 22:55:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,57 @@ app.get('/employees', async (request, response) => {
     
 });
 
+app.post('/employees', async (request, response) => {
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const department_id = req.body.department_id;
+});
+
+app.get('/departments', async (request, response) => {
+    try {
+        const [data, fields] = await connection.promise().query('SELECT * FROM fsbootcamp2024.departments');
+        return response.json(data);
+    } catch {
+        return response.send(errors);
+    }
+
+    console.log(data);
+    
+});
+
+app.get('/employees/:id', async (request, response) => {
+    const employee_id = request.params.id;
+    try {
+        const [data, fields] = await connection.promise().query('SELECT * FROM fsbootcamp2024.employees WHERE employee_id = ?', employee_id);
+        if (data.length > 0) {
+            return response.json(data);
+        } else {
+            return response.json('Employee not found.');
+        }
+    } catch {
+        return response.send(errors);
+    }
+
+    console.log(data);
+    
+});
+
+app.delete('/employees/delete/:id', async(request, response) => {
+    const employee_id = request.params.id;
+    try {
+        const [data, fields] = await connection.promise().query('DELETE FROM fsbootcamp2024.employees WHERE employee_id = ?', employee_id);
+        if (data.affectedRows > 0) {
+            return response.json('Employee has been deleted');
+        } else {
+            return response.json('Employee not found.');
+        }
+    } catch {
+        return response.send(errors);
+    }
+
+    console.log(data);
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
